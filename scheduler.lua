237c7f4fb01822cc4b3417fe94b4b87d99af65cb
Nick Terry <j.nick.terry@gmail.com>
2014-09-18 15:31:28 -0700

    Everything is working

--- scheduler.lua
+++ scheduler.lua
@@ -1,46 +1,114 @@
 child_by_co = {}
 child_by_id = {}
+id_count = 0
 
-function _me()
-  return child_by_co[coroutine.running()]
+function sprintf(f, ...)
+  return string.format(f, ...)
+end
+
+function printf(f, ...)
+  print(sprintf(f, ...))
+end
+
+function dprintf(...)
+  local info = debug.getinfo(2, "nSl")
+  local co = coroutine.running()
+  local child = child_by_co[co]
+  if child == nil then
+    name = "scheduler"
+  else
+    name = child.Name
+  end
+  printf("%s:%d:%s:%s %s", info.source, info.currentline, info.name, name, sprintf(...))
+end
+dprintf = function(...)end
+
+function _make_selector(me, who)
+  local not_me = function(child)
+    dprintf("not me %d, %s", me.Id, child.Name)
+    return child.Id ~= me.Id
+  end
+
+  dprintf("who: %s", tostring(who))
+  if who == "*" then
+    dprintf("selector not_me %s", tostring(not_me))
+    return not_me
+
+  elseif type(who) == "string" then
+    dprintf("selector kind")
+    return function(child)
+      dprintf("kind select %s == %s for %s", who, child.Name, me.Name)
+      return (child.Kind == who) and not_me(child)
+    end
+
+  else
+    dprintf("selector id")
+    return function(child)
+      dprintf("id select %d, %s", who, child.Name)
+      return child.Id == who
+    end
+  end
 end
 
 -- Child API
 
-function id()
-  return _me().Id
+function me()
+  return child_by_co[coroutine.running()]
 end
 
-function who(id)
-  return child_by_id[id]
+function spawn(k, body)
+  local co = coroutine.create(body)
+  local id = id_count
+  id_count = id_count + 1
+  local child = {
+    Kind = k,
+    Id = id,
+    Name = sprintf("%s:%d", k, id),
+    Co = co,
+    Msgs = {},
+    Waiter = function() return true end,
+  }
+  child_by_co[co] = child
+  child_by_id[id] = child
+  dprintf("Spawned %s", child.Name)
+  return child
 end
 
-function say(s)
-  me = _me()
-  print(string.format("%s(%d) says %s", me.Type, me.Id, s))
+function say(f, ...)
+  local me = me()
+  printf("%s says %q", me.Name, sprintf(f, ...))
 end
 
 function recv()
-  msgs = _me().Msgs
-  if msgs > 0 then
-    return table.remove(msgs, 0)
+  local msgs = me().Msgs
+  if next(msgs) then
+    return table.remove(msg, 1)
   end
   return nil
 end
 
-function send( who, what)
-  me = _me()
-  table.insert(child_by_id[who], {Src = me.Id, Msg = what})
+function send(who, what)
+  local me = me()
+  local selector = _make_selector(me, who)
+  dprintf("%s sending %q to %s", me.Name, what, who)
+  for co, child in pairs(child_by_co) do
+    if selector(child) then
+      printf("%s tells %s: %q", me.Name, child.Name, what)
+      table.insert(child.Msgs, {Src = me, Msg = what})
+    end
+  end
 end
 
-function or_( a, b )
+function any(...)
+  local waiters = {...}
   return function()
-    ready, extra = a()
-    if ready then
-      return true, 1, extra
+    for i, waiter in ipairs(waiters) do
+      local ready, resp = waiter()
+      if ready then
+        return true, i, resp
+      end
     end
-    ready, extra = b()
-    return ready, 2, extra
+    return false, nil, nil
   end
 end
 
@@ -49,11 +117,17 @@ function any_msg()
 end
 
 function msg_from(who)
-  me = _me()
+  local me = me()
+
+  dprintf("msg_from(%s)", tostring(who))
+  local selector = _make_selector(me, who)
+  dprintf("msg_from(%s) %s", tostring(who), tostring(selector))
   return function()
+    dprintf("%s<%d> is waiting for %s", me.Name, #me.Msgs, tostring(who))
     for i, msg in ipairs(me.Msgs) do
-      if msg.Src == who or who == "*" then
-        table.remove(me.Msgs,i)
+      if selector(msg.Src) then
+        printf("%s hears %s %q", me.Name, msg.Src.Name, msg.Msg)
+        table.remove(me.Msgs, i)
         return true, msg
       end
     end
@@ -73,32 +147,53 @@ function seconds(s)
 end
 
 function wait( waiter )
-  coroutine.yield(waiter)
+  return coroutine.yield(waiter)
 end
 
 -- Start Scheduler stuff
 
-for i, name in ipairs(arg) do
-  print( "loading file", i, name )
-  f = loadfile(name)
-  co = coroutine.create(f)
-  child = {Type=name, Co=co, Msgs={}, Id=i, Waiter=function()return true end}
-  child_by_co[co] = child
-  child_by_name[name] = child
-  child_by_id[i] = child
-end
+function scheduler()
+  for i, file in ipairs(arg) do
+    dprintf("loading file %q as %d", file, i)
+    local f = loadfile(file)
+    local k = file:sub(1, -5)
+    spawn(k, f)
+  end
 
-while next(child_by_co) do
-  print("scheduler tick")
-  for co, child in pairs(child_by_co) do
-    if child.Waiter == nil then
-      print(child.Type, child.Id, "exited")
-      child_by_co[co] = nil
-    else
-      ready, extra = child.Waiter()
-      if ready then
-        success, child.Waiter = coroutine.resume(co,extra)
+
+  local max_stall =100 
+  local stall = 0
+  while next(child_by_co) and stall < max_stall do
+    dprintf("scheduler tick")
+    for _, child in pairs(child_by_co) do
+      if child.Waiter == nil then
+        printf("%s is %s", child.Name, coroutine.status(child.Co))
+        child_by_co[child.Co] = nil
+        child.Co = nil
+      else
+        dprintf("checking child %s, %s", child.Name, tostring(child.Waiter))
+        local function waiter_check(ready, ...)
+          if ready then
+            local success
+            success, child.Waiter = coroutine.resume(child.Co, ...)
+            if not success then
+              printf("%s had an error %q", child.Name, child.Waiter)
+              child.Waiter = nil
+            end
+            stall = 0
+          end
+        end
+        waiter_check(child.Waiter())
       end
+      dprintf("done with child %s", child.Name)
     end
+    stall = stall + 1
+  end
+  if stall >= max_stall then
+    printf("Scheduler stalled")
+  else
+    printf("All children completed")
   end
 end
+
+scheduler()
